ce6aab52838c34b80077f0306177df18668dcbc0
Dallas Brooks <47010213+dallasbrooks@users.noreply.github.com>
2021-01-09 16:19:03 -0800

    first bigger commit
    adding bulk of my content, and css

--- js/utility.js
+++ js/utility.js
@@ -1,5 +1,12 @@
 //https://www.iconfinder.com/search/?q=gmail&style=flat
 
+$(document).ready(function() {
+	$(this).scrollTop(0);
+	$(".proj-header:first").toggleClass("active");
+	$(".proj-header:first").next(".proj-content").slideToggle("slow");
+	$(".proj-header:first").next(".proj-content").toggleClass("show");
+});
+
 $(".nav-bar").click(function(){
 	if($(this).text() == "Dallas Brooks" || $(this).text() == "Home"){
 		console.log("here");
@@ -8,4 +15,13 @@ $(".nav-bar").click(function(){
 	}
 	$("#"+$(this).text().toLowerCase()).css("display", "block");
 	$(".page").not("#"+$(this).text().toLowerCase()).css("display", "none");
+});
+
+$(".proj-header").click(function() {
+	$(this).toggleClass("active");
+	$(this).next(".proj-content").slideToggle("slow");
+	$(this).next(".proj-content").toggleClass("show");
+	$(".proj-header").not(this).next(".proj-content").slideUp("slow");
+	$(".proj-header").not(this).next(".proj-content").removeClass("show");
+	$(".proj-header").not(this).removeClass("active");
 });
